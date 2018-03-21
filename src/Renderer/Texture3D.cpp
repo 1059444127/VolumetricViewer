cdacb5c4446be963bba2a4d28e33188a6dffb44f
Reuben Carter <reubenjcarter@gmail.com>
2018-03-21 23:37:51 +0000

    Added to the texture 3d component

--- src/Renderer/Texture3D.cpp
+++ src/Renderer/Texture3D.cpp
@@ -1,7 +1,46 @@
 #include "Texture3D.hpp"
 
+#include <QtGui/QOpenGLFunctions>
+#include <QOpenGLFunctions_3_2_Compatibility>
+
 
 Texture3D::Texture3D()
+{
+	QOpenGLFunctions_3_2_Compatibility* ogl = QOpenGLContext::currentContext()->versionFunctions<QOpenGLFunctions_3_2_Compatibility>();
+	
+	ogl->glGenTextures(1, &textureId);
+	
+	ogl->glBindTexture(GL_TEXTURE_3D, textureId);
+	
+	ogl->glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	ogl->glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	ogl->glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	ogl->glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	ogl->glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+	
+	ogl->glBindTexture(GL_TEXTURE_3D, 0);
+}
+
+void Texture3D::Allocate(uint64_t w, uint64_t h, uint64_t d)
+{
+	QOpenGLFunctions_3_2_Compatibility* ogl = QOpenGLContext::currentContext()->versionFunctions<QOpenGLFunctions_3_2_Compatibility>();
+
+	ogl->glBindTexture(GL_TEXTURE_3D, textureId);
+	ogl->glCompressedTexImage3D(GL_TEXTURE_3D, 1, GL_RGBA8, width, height);
+	ogl->glBindTexture(GL_TEXTURE_3D, 0);
+}
+
+void Texture3D::LoadData(void* buffer, uint64_t count)
+{
+	QOpenGLFunctions_3_2_Compatibility* ogl = QOpenGLContext::currentContext()->versionFunctions<QOpenGLFunctions_3_2_Compatibility>();
+
+	ogl->glBindTexture(GL_TEXTURE_3D, textureId);
+	ogl->glTexSubImage3D(GL_TEXTURE_3D, 0, 0, 0, width, height, GL_BGRA, GL_UNSIGNED_BYTE, buffer);
+	//glCompressedTexSubImage2D
+	ogl->glBindTexture(GL_TEXTURE_3D, 0);
+}
+
+void Texture3D::LoadDataCompressed(void* buffer, uint64_t count)
 {
 	
 }
--- src/Renderer/Texture3D.hpp
+++ src/Renderer/Texture3D.hpp
@@ -7,5 +7,15 @@
 class Texture3D
 {
 	public:
+		unsigned int textureId; 
+		uint64_t width;
+		uint64_t height; 
+		uint64_t depth; 
+		void* buffer;
+		
 		Texture3D();
+		void Allocate(uint64_t w, uint64_t h, uint64_t d);
+		void AllocateCompressed(uint64_t w, uint64_t h, uint64_t d);
+		void LoadData(void* buffer, uint64_t count);
+		void LoadDataCompressed(void* buffer, uint64_t count);
 };
